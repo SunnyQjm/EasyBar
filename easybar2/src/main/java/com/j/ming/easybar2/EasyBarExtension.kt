cfacf6f2979ea7fe4bba0ba6a7115cce0dca5c43
MingJ <qjm253@gmail>
2018-05-14 08:49:11 +0800

    添加是否覆盖回调的可选项

--- easybar2/src/main/java/com/j/ming/easybar2/EasyBarExtension.kt
+++ easybar2/src/main/java/com/j/ming/easybar2/EasyBarExtension.kt
@@ -14,12 +14,12 @@ class EasyBarParams(val mode: EasyBar.Mode = EasyBar.Mode.ICON_, val title: Stri
                         println("empty right")
                     }, @DrawableRes val leftRes: Int = R.drawable.back,
                     @DrawableRes val rightRes: Int = R.drawable.icon_add, val leftText: String = "",
-                    val rightText: String = "", @StringRes val titleRes: Int = -1){
+                    val rightText: String = "", @StringRes val titleRes: Int = -1, val isCoverCallback: Boolean = true){
 }
 
 fun EasyBar.init(easyBarParams: EasyBarParams){
     init(easyBarParams.mode, easyBarParams.title, easyBarParams.leftCallback, easyBarParams.rightCallback, easyBarParams.leftRes,
-            easyBarParams.rightRes, easyBarParams.leftText, easyBarParams.rightText, easyBarParams.titleRes)
+            easyBarParams.rightRes, easyBarParams.leftText, easyBarParams.rightText, easyBarParams.titleRes, easyBarParams.isCoverCallback)
 }
 
 /**
@@ -31,7 +31,7 @@ fun EasyBar.init(mode: EasyBar.Mode = EasyBar.Mode.ICON_, title: String = "", le
                      println("empty right")
                  }, @DrawableRes leftRes: Int = R.drawable.back,
                  @DrawableRes rightRes: Int = R.drawable.icon_add, leftText: String = "",
-                 rightText: String = "", @StringRes titleRes: Int = -1) {
+                 rightText: String = "", @StringRes titleRes: Int = -1, isCoverCallback: Boolean = true) {
     this.setDisplayMode(mode)
     if (titleRes > 0)
         this.setTitle(titleRes)
@@ -41,17 +41,15 @@ fun EasyBar.init(mode: EasyBar.Mode = EasyBar.Mode.ICON_, title: String = "", le
     this.setLeftText(leftText)
     this.setRightText(rightText)
     this.setLeftIcon(leftRes)
-    this.setOnEasyBarClickListener(object : EasyBar.OnEasyBarClickListener {
-        override fun onLeftIconClick(view: View) {
-            println("left click")
-            leftCallback(view)
-        }
+    if(isCoverCallback)
+        this.setOnEasyBarClickListener(object : EasyBar.OnEasyBarClickListener {
+            override fun onLeftIconClick(view: View) {
+                leftCallback(view)
+            }
 
-        override fun onRightIconClick(view: View) {
-            println("right click: ${rightCallback.hashCode()}")
-            rightCallback.invoke(view)
-            println("right click finish: ${view.hashCode()}")
-        }
+            override fun onRightIconClick(view: View) {
+                rightCallback.invoke(view)
+            }
 
-    })
+        })
 }
