4533cb4bd0ca9c587801b4ae64fd162eeae07239
MingJ <qjm253@gmail>
2018-05-14 08:29:28 +0800

    添加EasyBar初始化参数体

--- easybar2/src/main/java/com/j/ming/easybar2/EasyBarExtension.kt
+++ easybar2/src/main/java/com/j/ming/easybar2/EasyBarExtension.kt
@@ -9,18 +9,31 @@ import android.view.View
  * Created by sunny on 18-1-25.
  */
 
-@SuppressLint("ResourceType")
-        /**
+class EasyBarParams(val mode: EasyBar.Mode = EasyBar.Mode.ICON_, val title: String = "", val leftCallback: (v: View) -> Unit = {},
+                    val rightCallback: (v: View) -> Unit = {
+                        println("empty right")
+                    }, @DrawableRes val leftRes: Int = R.drawable.back,
+                    @DrawableRes val rightRes: Int = R.drawable.icon_add, val leftText: String = "",
+                    val rightText: String = "", @StringRes val titleRes: Int = -1){
+}
+
+fun EasyBar.init(easyBarParams: EasyBarParams){
+    init(easyBarParams.mode, easyBarParams.title, easyBarParams.leftCallback, easyBarParams.rightCallback, easyBarParams.leftRes,
+            easyBarParams.rightRes, easyBarParams.leftText, easyBarParams.rightText, easyBarParams.titleRes)
+}
+
+/**
  * 标题栏初始化
  */
-fun EasyBar.init(mode: EasyBar.Mode = EasyBar.Mode.ICON_, title: String = "", leftCallback: (v: View)->Unit = {},
-                 rightCallback: (v: View)->Unit = {
+@SuppressLint("ResourceType")
+fun EasyBar.init(mode: EasyBar.Mode = EasyBar.Mode.ICON_, title: String = "", leftCallback: (v: View) -> Unit = {},
+                 rightCallback: (v: View) -> Unit = {
                      println("empty right")
                  }, @DrawableRes leftRes: Int = R.drawable.back,
                  @DrawableRes rightRes: Int = R.drawable.icon_add, leftText: String = "",
-                 rightText: String = "", @StringRes titleRes: Int = -1){
+                 rightText: String = "", @StringRes titleRes: Int = -1) {
     this.setDisplayMode(mode)
-    if(titleRes > 0)
+    if (titleRes > 0)
         this.setTitle(titleRes)
     else
         this.setTitle(title)
@@ -28,7 +41,7 @@ fun EasyBar.init(mode: EasyBar.Mode = EasyBar.Mode.ICON_, title: String = "", le
     this.setLeftText(leftText)
     this.setRightText(rightText)
     this.setLeftIcon(leftRes)
-    this.setOnEasyBarClickListener(object : EasyBar.OnEasyBarClickListener{
+    this.setOnEasyBarClickListener(object : EasyBar.OnEasyBarClickListener {
         override fun onLeftIconClick(view: View) {
             println("left click")
             leftCallback(view)
